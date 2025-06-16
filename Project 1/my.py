9377cd6d8ccf95030138c492d7373ab91e64e44e
LeTHaLOp17 <aksingh6224@gmail.com>
2025-06-16 11:00:08 +0530

    Rock Paper Scissor game

---
+++ Project 1/my.py
@@ -0,0 +1,17 @@
+import random
+
+'''
+-1 = Rock
+0 = Paper
+1 = Scissor
+
+'''
+computer = random.choice([-1, 0, 1])
+choiceDict = {'R' : -1, 'P' : 0, 'S' : 1}
+revDict = {-1 : 'Rock', 0 : 'Paper', 1 : 'Siccor'}
+youstr = input("Enter your choice (R for Rock, P for Paper, S for Sciccor): ").upper()
+if youstr not in choiceDict:
+    print("Invalid input!")
+    exit()
+
+you = choiceDict[youstr]
