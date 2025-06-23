b30e4e6c3eb5c7ed1ba5c5ee6bf4b7be4d95e9e1
LeTHaLOp17 <aksingh6224@gmail.com>
2025-06-23 12:35:42 +0530

    Updated

--- Project 1/my.py
+++ Project 1/my.py
@@ -8,10 +8,37 @@ import random
 '''
 computer = random.choice([-1, 0, 1])
 choiceDict = {'R' : -1, 'P' : 0, 'S' : 1}
-revDict = {-1 : 'Rock', 0 : 'Paper', 1 : 'Scissor'}
+reverseDict = {-1 : 'Rock', 0 : 'Paper', 1 : 'Scissor'}
 youstr = input("Enter your choice (R for Rock, P for Paper, S for Scissor): ").upper()
 if youstr not in choiceDict:
     print("Invalid input!")
     exit()
 
 you = choiceDict[youstr]
+
+print(f"You chose {reverseDict[you]}\nComputer chose {reverseDict[computer]}")
+
+if(computer == you):
+    print("Its a draw")
+
+else:
+    if(computer == -1 and you == 1): 
+        print("You Lose!")
+
+    elif(computer == -1 and you == 0):
+        print("You Win!")
+
+    elif(computer == 1 and you == -1):
+        print("You Win!")
+
+    elif(computer == 1 and you == 0):
+        print("You Lose!")
+
+    elif(computer == 0 and you == -1):
+        print("You Lose!")
+
+    elif(computer == 0 and you == 1):
+        print("You Win!")
+
+    else:
+        print("Something went wrong!")
